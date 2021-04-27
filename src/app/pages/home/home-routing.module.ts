4547dad18b5d75a0580effedd8e4a54c038dc6ae
DiegoTafurSan13 <tafur232@gmail.com>
2021-04-27 13:30:22 -0500

    add carrousel

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'index',
+    component:HomeComponent
+  },
+  {
+    path:'',
+    redirectTo:'index',
+    pathMatch:'full'
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarrouselComponent } from './carrousel/carrousel.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CarrouselComponent],
   imports: [
     CommonModule
+  ],
+  exports:[
+    CarrouselComponent
   ]
 })
 export class SharedModule { }
