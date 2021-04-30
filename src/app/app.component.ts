be5eaf6a5612654a9b83a3ed56d83c1fc97377c5
Flavio <flavio.svenps@gmail.com>
2021-04-30 18:26:33 -0500

    accesos directos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portal-barranca';
+
+  data: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.addData();
+  }
+
+addData() {
+    this.data = [
+      {
+        title: 'FORTALECIENDO LA GESTIÓN DE LAS ORGANIZACIONES SOCIALES',
+        info: '#JuntosSíPodemos | Programa virtual de formación: Fortaleciendo la gestión de las organizaciones sociales.'
+      },
+      {
+        title: 'LABORATORIO SOCIAL: CREA TU ORGANIZACIÓN JUVENIL',
+        info: '#ProgramaVirtual Laboratorio social: Crea tu organización juvenil.'
+      },
+      {
+        title: 'TALLERES DE VERANO - EDICIÓN VIRTUAL 2021',
+        info: '#ProgramaVirtual Laboratorio social: Crea tu organización juvenil.'
+      }
+    ]
+  }
 }
--- src/app/shared/accesos-directos/accesos-directos.component.ts
+++ src/app/shared/accesos-directos/accesos-directos.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccesosDirectosComponent implements OnInit {
 
+  data: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.addData();
+  }
+
+addData() {
+    this.data = [
+      {
+        title: 'FORTALECIENDO LA GESTIÓN DE LAS ORGANIZACIONES SOCIALES',
+        info: '#JuntosSíPodemos | Programa virtual de formación: Fortaleciendo la gestión de las organizaciones sociales.'
+      },
+      {
+        title: 'LABORATORIO SOCIAL: CREA TU ORGANIZACIÓN JUVENIL',
+        info: '#ProgramaVirtual Laboratorio social: Crea tu organización juvenil.'
+      },
+      {
+        title: 'TALLERES DE VERANO - EDICIÓN VIRTUAL 2021',
+        info: '#ProgramaVirtual Laboratorio social: Crea tu organización juvenil.'
+      }
+    ]
   }
 
 }
